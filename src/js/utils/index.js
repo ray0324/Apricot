64457eb534b169fc606cf4055a3e5f18de55a932
ray0324 <ray0324@foxmail.com>
2018-12-21 17:14:59 +0800

    update

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -6,7 +6,7 @@ define(function (require) {
   var parseMenu = function (menuList) {
     var menu_links = menuConf.menu_links;
     return menuList.map(function (menu) {
-      menu.PROG_ID = menu_links[menu.PROG_ID] || '#';
+      menu.PROG_ID = menu_links[menu.PROG_ID] || '/404';
       return menu;
     })
   }
